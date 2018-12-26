4fb6031634cb1da9b4a6415dca773b1a02b40f1d
paulwrodriguez <paul@admin.com>
2018-12-25 23:58:08 -0700

    added game info section

--- sketch.js
+++ sketch.js
@@ -3,13 +3,16 @@
 var h = 600;
 var w = 600;
 var board;
+var hud;
 
 function setup() {
   createCanvas(w, h);
   board = new Board(w,h);
+  hud = new HUD(w,h);
 }
 
 function draw() {
   board.display();
+  hud.display();
 }
 

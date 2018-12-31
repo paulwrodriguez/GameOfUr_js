138aadf550456f07a94c3f26be6a9b54544b947c
paulwrodriguez <paul@admin.com>
2018-12-30 22:48:37 -0700

    issue with inputbox value undefined

--- sketch.js
+++ sketch.js
@@ -9,7 +9,25 @@ function setup() {
   createCanvas(w, h);
   board = new Board(w,h);
   hud = new HUD(w,h);
+  // createInputThing();
 }
+
+// function createInputThing() {
+//     input = createInput();
+//     input.position(w/5.5, h/8);
+    
+//     button = createButton('submit');
+//     button.position(w / 6,h / 7);
+    
+//     button.mousePressed(test);
+    
+// }
+
+function test() {
+  input.value(' ');
+  console.log("hello world");
+}
+
 
 function draw() {
   board.display();
